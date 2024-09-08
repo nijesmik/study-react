1e86e98fa385c96a4db241ae7e98ae22658ee137
nijesmik <bkybb@duck.com>
2024-09-09 02:14:15 +0900

    :bug: node 삭제 시 index가 감소하는 문제 수정

--- src/lib/dom/updateElement.js
+++ src/lib/dom/updateElement.js
@@ -4,13 +4,15 @@ import { isEventAttribute, setAttribute } from "./attribute.js";
 export const updateElement = (parent, currentVNode, newVNode, index = 0) => {
   // 1. newNode만 있는 경우
   if (!isExist(currentVNode) && isExist(newVNode)) {
-    return parent.appendChild(createElement(newVNode));
+    parent.appendChild(createElement(newVNode));
+    return;
   }
 
   // 2. currentNode만 있는 경우
   const node = parent.childNodes[index];
   if (isExist(currentVNode) && !isExist(newVNode)) {
-    return parent.removeChild(node);
+    parent.removeChild(node);
+    return index;
   }
 
   // 3. currentNode와 newNode 모두 text 타입일 경우
@@ -18,12 +20,14 @@ export const updateElement = (parent, currentVNode, newVNode, index = 0) => {
     if (currentVNode === newVNode) {
       return;
     }
-    return parent.replaceChild(createElement(newVNode), node);
+    parent.replaceChild(createElement(newVNode), node);
+    return;
   }
 
   // 4. currentNode와 newNode의 태그 이름(type)이 다를 경우
   if (currentVNode.type !== newVNode.type) {
-    return parent.replaceChild(createElement(newVNode), node);
+    parent.replaceChild(createElement(newVNode), node);
+    return;
   }
 
   // 5. currentNode와 newNode의 태그 이름(type)이 같을 경우
@@ -35,8 +39,9 @@ export const updateElement = (parent, currentVNode, newVNode, index = 0) => {
 
 export const updateElements = (parent, currentVNodes, newVNodes) => {
   const maxLength = Math.max(currentVNodes.length, newVNodes.length);
+  let index = undefined;
   for (let i = 0; i < maxLength; i++) {
-    updateElement(parent, currentVNodes[i], newVNodes[i], i);
+    index = updateElement(parent, currentVNodes[i], newVNodes[i], index ?? i);
   }
 };
 
