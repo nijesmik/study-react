fdc68cfe465fee6bd33256c966e751921ad965eb
nijesmik <bkybb@duck.com>
2024-09-10 22:50:21 +0900

    :sparkles: useSearchParams

--- src/lib/router/hooks.js
+++ src/lib/router/hooks.js
@@ -3,3 +3,6 @@ import { $router } from "./Router.js";
 export const useNavigate = () => (path, options) => {
   $router.navigate(path, options);
 };
+
+export const useSearchParams = () =>
+  new URLSearchParams(window.location.search);
