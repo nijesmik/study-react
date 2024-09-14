ae7c2256b7258f6040d3989b6ae678b1d4e62b7b
nijesmik <bkybb@duck.com>
2024-09-14 16:31:21 +0900

    :fire: 테스트 코드 삭제

--- src/main.jsx
+++
@@ -1,8 +0,0 @@
-import "./styles/global.css";
-import { virtualDOM } from "./lib/dom/VirtualDOM.js";
-import { RouterProvider } from "./lib/router/RouterProvider.js";
-import { routes } from "./routes.jsx";
-
-virtualDOM
-  .createRoot(document.getElementById("root"))
-  .render(<RouterProvider routes={routes} />);
