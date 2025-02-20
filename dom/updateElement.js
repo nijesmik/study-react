f818a8bfb0f29e65bd5ea4dcefe3503754ee033d
nijesmik <bkybb@duck.com>
2025-02-21 06:57:59 +0900

    :recycle: updateElement()

--- dom/updateElement.js
+++ dom/updateElement.js
@@ -1,72 +1,63 @@
 import { createElement } from "./createElement.js";
 import { updateProperties } from "./bindings/client/DOMComponent.js";
 
-export const updateElement = (parent, prevVNode, newVNode, index = 0) => {
+const updateElement = (parentElement, prevNode, newNode, index) => {
   // 1. newNode만 있는 경우 -> node 추가
-  if (!isExist(prevVNode) && isExist(newVNode)) {
-    parent.appendChild(createElement(newVNode));
+  if (!exist(prevNode) && exist(newNode)) {
+    parentElement.appendChild(createElement(newNode));
     return;
   }
+
+  const domElement = parentElement.childNodes[index];
 
   // 2. newNode가 없는 경우 -> node 삭제
-  if (!isExist(newVNode)) {
-    return deleteElement(parent, prevVNode, index);
+  if (!exist(newNode)) {
+    parentElement.removeChild(domElement);
+    return true;
   }
 
-  // 3. prevNode와 newNode 모두 text 타입일 경우
-  if (isTextNode(prevVNode) && isTextNode(newVNode)) {
-    return updateTextElement(parent, prevVNode, newVNode, index);
-  }
-
-  // 4. prevNode와 newNode의 태그 이름(type)이 다를 경우
-  const node = parent.childNodes[index];
-  if (prevVNode.type !== newVNode.type) {
-    if (node) {
-      parent.replaceChild(createElement(newVNode), node);
-      return;
+  // 3. prevNode와 newNode 모두 text node일 경우
+  if (isTextNode(prevNode) && isTextNode(newNode)) {
+    if (prevNode !== newNode) {
+      parentElement.replaceChild(createElement(newNode), domElement);
     }
-    parent.appendChild(createElement(newVNode));
     return;
   }
 
-  // 5. prevNode와 newNode의 태그 이름(type)이 같을 경우
+  // 4. prevNode와 newNode의 태그 종류가 다를 경우
+  if (prevNode.type !== newNode.type) {
+    parentElement.replaceChild(createElement(newNode), domElement);
+    return;
+  }
+
+  // 5. prevNode와 newNode의 태그 종류가 같을 경우
   updateProperties(
-    node,
-    newVNode.type,
-    prevVNode.props ?? {},
-    newVNode.props ?? {},
+    domElement,
+    newNode.type,
+    prevNode.props ?? {},
+    newNode.props ?? {},
   );
 
-  // 6. newNode와 prevNode의 모든 자식 태그를 순회하며 1 ~ 5의 내용을 반복한다.
-  updateElements(node, prevVNode.children, newVNode.children);
+  // 6. newNode와 prevNode의 children을 순회하며 1 ~ 5의 내용을 반복한다.
+  updateElements(domElement, prevNode.children, newNode.children);
 };
 
-const deleteElement = (parent, prevVNode, index) => {
-  if (!isExist(prevVNode)) {
-    return;
-  }
-  const node = parent.childNodes[index];
-  parent.removeChild(node);
-  return index;
-};
-
-export const updateTextElement = (parent, prevVNode, newVNode, index) => {
-  if (prevVNode === newVNode) {
-    return;
-  }
-  const node = parent.childNodes[index];
-  parent.replaceChild(createElement(newVNode), node);
-};
-
-export const updateElements = (parent, prevVNodes, newVNodes) => {
-  const maxLength = Math.max(prevVNodes?.length, newVNodes?.length);
-  let index = undefined;
-  for (let i = 0; i < maxLength; i++) {
-    index = updateElement(parent, prevVNodes[i], newVNodes[i], index ?? i);
+export const updateElements = (parentElement, prevNodes, newNodes) => {
+  const maxLength = Math.max(prevNodes.length, newNodes.length);
+  for (let i = 0, j = 0; i < maxLength; i++, j++) {
+    const isDeleted = updateElement(
+      parentElement,
+      prevNodes[i],
+      newNodes[i],
+      j,
+    );
+    if (isDeleted) {
+      j--;
+    }
   }
 };
 
-const isExist = (node) => {
+const exist = (node) => {
   return node !== null && node !== undefined;
 };
 
